a004e280de4c4216bf678f6dcd8fd8401fed97d4
Shayan Karki <shayankarki@gmail.com>
2025-10-28 19:31:02 -0500

    Django User: admin, password: unionhub

---
+++ uniohub_backend/formapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import JsonResponse
+from .models import UserSubmission
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt  # allows testing from plain HTML without CSRF
+def submit_form(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        message = request.POST.get('message', '')
+        UserSubmission.objects.create(name=name, email=email, message=message)
+        return JsonResponse({'success': True})
+    return JsonResponse({'success': False, 'error': 'POST required'})
