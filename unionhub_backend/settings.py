34881cc7e7e87f137a6a909b5c195580dcbd1e28
Shayan Karki <shayankarki@gmail.com>
2025-12-02 18:27:08 -0600

    final

--- unionhub_backend/settings.py
+++ unionhub_backend/settings.py
@@ -24,19 +24,16 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.up.railway.app",
 ]
 
-# -----------------------------------------
-# STATIC FILES  (Correct for Railway)
-# -----------------------------------------
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# Leave empty — Django will find formapp/static/formapp/* automatically
-STATICFILES_DIRS = []
 
-# -----------------------------------------
-# MEDIA FILES
-# -----------------------------------------
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
@@ -58,6 +55,7 @@ INSTALLED_APPS = [
 # -----------------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
